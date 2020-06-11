24493bdffab38a5fcf1aeb4cb960f93306023434
Eric <eric.halonen1@gmail.com>
2020-06-11 16:35:40 -0400

    julia extension on VS doesnt work atm

--- be/src/server.jl
+++ be/src/server.jl
@@ -1,8 +1,10 @@
 using Plots
 using Json
-using dekkdkdfjcrlwnvdlkwfnql   fead
+using HTTP
 
-function velocityPLot(){
+
+function velocityPLot()
     x = 1:10; y = rand(10); # These are the plotting data
     return plot(x, y)
-}
+end
+
