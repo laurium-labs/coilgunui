8b820bca27c2d384655613f6f96702019805520a
Eric <eric.halonen1@gmail.com>
2020-06-12 16:38:27 -0400

    merge

--- be/src/server.jl
+++ be/src/server.jl
@@ -2,46 +2,37 @@ using Plots
 using JSON
 using HTTP
 using Joseki
-using JuliaWebAPI
-
-# Define functions testfn1 and testfn2 that we shall expose
-function testfn1(arg1, arg2; narg1="1", narg2="2")
-    return (parse(Int, arg1) * parse(Int, narg1)) + (parse(Int, arg2) * parse(Int, narg2))
-end
-
-testfn2(arg1, arg2; narg1="1", narg2="2") = testfn1(arg1, arg2; narg1=narg1, narg2=narg2)
-
-# Expose testfn1 and testfn2 via a ZMQ listener
-process(
-    JuliaWebAPI.create_responder([
-        (testfn1, true),
-        (testfn2, false)
-    ], "tcp://127.0.0.1:9999", true, "")
-)
+import HTTP.IOExtras.bytes
 
 function velocityPlot()
     x = 1:10; y = rand(10); # These are the plotting data
     return x,y
 end
 
-function auth_preproc(req::HTTP.Request)
+function simulate(req::HTTP.Request)
     if !validate(req)
         return HTTP.Response(401)
     end
     return velocityPlot()
 end
+
+function params(req::HTTP.Request)
+    b = get_default_scenario_json()
+    req.response.body = bytes(b)
+    return req.response
+end
+
+
 run_http(apiclnt, 8888, auth_preproc)
-# r = Joseki.router(endpoints)
+endpoints = [
+    (simulate, "GET", "/simulate"),
+    (params, "GET", "/params")
+]
+
+r = Joseki.router(endpoints)
 velocityPlot()
 
-<<<<<<< Updated upstream
-
-# haskey(ENV, "PORT") ? port = parse(Int32, ENV["PORT"]) : port = 8000
-
-# HTTP.serve(r, "0.0.0.0", port; verbose=false)
-=======
  haskey(ENV, "PORT") ? port = parse(Int32, ENV["PORT"]) : port = 8000
 println("trees")
 HTTP.serve(r, "0.0.0.0", port; verbose=false)#Belive this creates n active API, shuts down when program does
 println("here")
->>>>>>> Stashed changes
