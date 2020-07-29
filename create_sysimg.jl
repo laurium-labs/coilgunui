158d7acf8b0a0a25265b1d6ae130fb319acaa884
Mark Halonen <halonen.mark@gmail.com>
2020-07-29 16:54:31 -0400

    try simpler so creator

--- create_sysimg.jl
+++ create_sysimg.jl
@@ -1,10 +1,6 @@
 using Pkg
 Pkg.add(PackageSpec(url="https://github.com/laurium-labs/CoilGun.jl"))
-Pkg.add("HTTP")
-Pkg.add("Joseki")
-Pkg.add("JSON")
-Pkg.add("PackageCompiler")
 
 using PackageCompiler
 
-create_sysimage([:CoilGun, :HTTP, :Joseki, :JSON]; precompile_statements_file="precompile_coilgunner", sysimage_path="sys_coilgunner.so")
+create_sysimage([:CoilGun]; precompile_execution_file="precompile_coilgun.jl", sysimage_path="sys_coilgunner.so")
