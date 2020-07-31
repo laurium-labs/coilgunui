2fdbd2bef3f974255a6d4b5728b8b36406b2e0b8
Mark Halonen <halonen.mark@gmail.com>
2020-07-31 15:18:58 -0400

    shim shady

---
+++ ShimServer/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+var request = require('request');
+
+app.use('/', function (req, res) {
+    var url = "http://34.237.210.243" + req.url;
+    req.pipe(request({ qs: req.query, uri: url })).pipe(res);
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
